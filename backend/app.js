a4b1578a2d9be2ffe9740c56b640ea381c94bd86
ab3nxy <abenxy0@gmail.com>
2024-04-14 17:52:55 +0300

    Ready for render 5

--- backend/app.js
+++ backend/app.js
@@ -15,10 +15,10 @@ app.use(express.json());
 app.use('/api/blogs', postsRouter);
 
 // Set static folder
-app.use(express.static(path.join(__dirname, 'frontend','build')));
+app.use(express.static(path.join(__dirname, '..', 'frontend','build')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname,'frontend', 'build', 'index.html'));
+  res.sendFile(path.resolve(__dirname, '..','frontend', 'build', 'index.html'));
 });
 
 const PORT = process.env.PORT || 5000;
