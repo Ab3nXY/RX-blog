431e288d9a34702cfd7532fb00fc836f42e155c9
ab3nxy <abenxy0@gmail.com>
2024-04-14 17:02:04 +0300

    Ready for render

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const postsRouter = require('./routes/blogs');
+const path = require('path');
 
 const app = express();
 
@@ -12,6 +13,13 @@ app.use(express.json());
 
 // Mounting the router for handling blog-related routes
 app.use('/api/blogs', postsRouter);
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'frontend','build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname,'frontend', 'build', 'index.html'));
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
